cb3e18c42787fd71b958876f9e56cb576a36e1c1
Lorthirash <88578121+Lorthirash@users.noreply.github.com>
2023-09-28 14:53:32 +0200

    Add Routing and Create Home, Error, Root, Product, SupplierInfo Pages

--- northwindfrontend/src/App.tsx
+++ northwindfrontend/src/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "./App.css";
-import AvailableProducts from './components/availableProducts/AvailableProducts'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AvailableProducts from "./components/availableProducts/AvailableProducts";
+import SupplierInfo from "./components/supplierInfo/SupplierInfo";
+import Home from "./components/Home/Home";
+import RootLayout from "./components/Root/Root";
+import ErrorPage from "./components/Error/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products", element: <AvailableProducts /> },
+      { path: "/supplierinfo", element: <SupplierInfo /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="container">
-     <AvailableProducts/>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- northwindfrontend/src/components/availableProducts/AvailableProducts.tsx
+++ northwindfrontend/src/components/availableProducts/AvailableProducts.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import productService from "../../services/ProductService"; // Az elérési út legyen a megfelelő helyre mutató
 import ProductModel from "../../models/ProductModel";
+import './AvailableProducts.css';
 
 function AvailableProduct() {
   const [products, setProducts] = useState<ProductModel[]>([]);
@@ -19,41 +20,41 @@ function AvailableProduct() {
   });
 
   return (
-    <div>
-      <h1 className="mt-4">Available product</h1>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Product ID</th>
-            <th scope="col">Product Name</th>
-            <th scope="col">Supplier ID</th>
-            <th scope="col">Category ID</th>
-            <th scope="col">Quantity Per Unit</th>
-            <th scope="col">Unit Price (USD)</th>
-            <th scope="col">Units In Stock</th>
-            <th scope="col">Units On Order</th>
-            <th scope="col">Reorder Level</th>
-            <th scope="col">Discontinued</th>
+    <div className="centered-container">
+    <h1 className="mt-4">Available product</h1>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Product ID</th>
+          <th scope="col">Product Name</th>
+          <th scope="col">Supplier ID</th>
+          <th scope="col">Category ID</th>
+          <th scope="col">Quantity Per Unit</th>
+          <th scope="col">Unit Price (USD)</th>
+          <th scope="col">Units In Stock</th>
+          <th scope="col">Units On Order</th>
+          <th scope="col">Reorder Level</th>
+          <th scope="col">Discontinued</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.productID}>
+            <td>{product.productID}</td>
+            <td>{product.productName}</td>
+            <td>{product.supplierID}</td>
+            <td>{product.categoryID}</td>
+            <td>{product.quantityPerUnit}</td>
+            <td>{product.unitPrice} USD</td>
+            <td>{product.unitsInStock} db</td>
+            <td>{product.unitsOnOrder}</td>
+            <td>{product.reorderLevel}</td>
+            <td>{product.discontinued ? "Yes" : "No"}</td>
           </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.productID}>
-              <td>{product.productID}</td>
-              <td>{product.productName}</td>
-              <td>{product.supplierID}</td>
-              <td>{product.categoryID}</td>
-              <td>{product.quantityPerUnit}</td>
-              <td>{product.unitPrice} USD</td>
-              <td>{product.unitsInStock} db</td>
-              <td>{product.unitsOnOrder}</td>
-              <td>{product.reorderLevel}</td>
-              <td>{product.discontinued ? "Yes" : "No"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
+  </div>
   );
 }
 
